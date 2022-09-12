e92afad7987f68a75bd6293a9df8898a096c076e
Barabaka <ivan.gopey@gmail.com>
2022-09-12 18:31:34 +0300

    Really only this can be considered like a first commit for this project

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const NOTES = [
+  {
+    noteId: 1,
+    noteName: "Arsenal Season Start",
+    creationDate: "2022-08-22T19:10:00Z",
+    category: "Idea",
+    noteContent:
+      "It should be good idea to get another midlefield because of high risk of new Thomas Party injury",
+    isArchived: false,
+  },
+  {
+    noteId: 2,
+    noteName: "Main principle behind 4 houses of Hogwarts ",
+    creationDate: "2022-01-11T06:23:09Z",
+    category: "Idea",
+    noteContent:
+      "It seems to me that 4 types of temperament is the main idea behind four different houses of Hogwarts",
+    isArchived: false,
+  },
+  {
+    noteId: 3,
+    noteName: "Winter Tyres",
+    creationDate: "2022-09-05T16:10:00Z",
+    category: "Task",
+    noteContent: "Don't forget to buy new winter Bridgestone Tyres",
+    isArchived: true,
+  },
+  {
+    noteId: 4,
+    noteName: "Rescheduling of my visit to dentist",
+    creationDate: "2022-07-12T18:02:00Z",
+    category: "Task",
+    noteContent:
+      "Need to plan three next visits to my dentist for 19/7/2022, 2/8/2022 та 28/11/2022",
+    isArchived: false,
+  },
+  {
+    noteId: 5,
+    noteName: "Make Donate for Pritula Foundation",
+    creationDate: "2022-02-25T12:02:00Z",
+    category: "Task",
+    noteContent: "Russian Warship go fuck yourself",
+    isArchived: true,
+  },
+  {
+    noteId: 6,
+    noteName: "Surf Thought 1",
+    creationDate: "2022-01-01T12:00:00Z",
+    category: "Random Thought",
+    noteContent:
+      "Ocean surf is good metaphore for usual life: you had to paddle 98% of time to have 2% enjoinhg of riding the wave",
+    isArchived: false,
+  },
+  {
+    noteId: 7,
+    noteName: "Croissant Rates",
+    creationDate: "2022-09-02T10:30:00Z",
+    category: "Random Thought",
+    noteContent: "1) Zavertailo; 2) Etre Sophie",
+    isArchived: false,
+  },
+];
